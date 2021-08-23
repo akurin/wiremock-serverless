9fdc1598b21bd477ffb8e9614adc0b1c29fc8a02
Alexey Kurin <alexey.kurin.nik@gmail.com>
2021-08-24 00:01:59 +0300

    Improve RoutingRequestHandlerTest

--- app/src/test/java/com/morjoff/wiremockserverless/RoutingRequestHandlerTest.java
+++ app/src/test/java/com/morjoff/wiremockserverless/RoutingRequestHandlerTest.java
@@ -1,21 +1,31 @@
 package com.morjoff.wiremockserverless;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class RoutingRequestHandlerTest {
+    private TestRequestHandler adminRequestHandler;
+    private TestRequestHandler stubRequestHandler;
+    private RoutingRequestHandler sut;
+
+    @BeforeEach
+    public void BeforeEach() {
+        this.adminRequestHandler = new TestRequestHandler();
+        this.stubRequestHandler = new TestRequestHandler();
+        this.sut = new RoutingRequestHandler(this.adminRequestHandler, this.stubRequestHandler);
+    }
+
     @Test
     public void should_route_admin_requests() {
-        TestRequestHandler adminRequestHandler = new TestRequestHandler();
-        TestRequestHandler stubRequestHandler = new TestRequestHandler();
-        RoutingRequestHandler sut = new RoutingRequestHandler(adminRequestHandler, stubRequestHandler);
+
         TestRequest request = new TestRequest();
         request.setUrl("/__admin/mappings/new");
         TestHttpResponder httpResponder = new TestHttpResponder();
 
-        sut.handle(request, httpResponder);
+        this.sut.handle(request, httpResponder);
 
         assertTrue(adminRequestHandler.wasCalled());
         assertFalse(stubRequestHandler.wasCalled());
@@ -23,14 +33,11 @@ public class RoutingRequestHandlerTest {
 
     @Test
     public void should_route_stub_requests() {
-        TestRequestHandler adminRequestHandler = new TestRequestHandler();
-        TestRequestHandler stubRequestHandler = new TestRequestHandler();
-        RoutingRequestHandler sut = new RoutingRequestHandler(adminRequestHandler, stubRequestHandler);
         TestRequest request = new TestRequest();
         request.setUrl("/some");
         TestHttpResponder httpResponder = new TestHttpResponder();
 
-        sut.handle(request, httpResponder);
+        this.sut.handle(request, httpResponder);
 
         assertFalse(adminRequestHandler.wasCalled());
         assertTrue(stubRequestHandler.wasCalled());
